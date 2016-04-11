55f68d85f1a58bdd7f4b35a84cf0d55e6c9cf04d
Thada Wangthammang <mildronize@dev-md9>
2016-04-11 16:40:57 +0700

    read from csv then write to opentsdb

--- csv_to_opentsdb.py
+++ csv_to_opentsdb.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python3
+import sys
+
 import csv
 import time
 import datetime
 
-def read_csv():
-    pass
+import requests
+
+# I/O
+def metric_send(metric,  value, tags, timestamp=int(time.time()) ):
+    url = 'http://172.30.235.192:4242/api/put'
+    data = {
+        "metric": metric,
+        "timestamp": timestamp,
+        "value": value,
+        "tags":  tags
+    }
+    print(data)
+    requests.post(url, data = data)
+
+def read_csv(path):
+    with open(path, 'r',encoding="utf-8") as f:
+        reader = csv.reader(f)
+        num_row = len(list(reader))
+        i = 0
+        for row in reader:
+            if i > 1:
+               result = row_convert(row)
+               #  metrics.send('level', result[])
+               metric_send( metric = 'level', \
+                       timestamp = result[0], \
+                       value =  result[1], \
+                       tags= {'location':'Klong Luek'})
+               print('%d %' % row*100/num_row)
+            i += 1
 
 def pad_zero_time(string_time):
     hours = int(string_time.split(':')[0])
@@ -30,15 +59,6 @@ def row_convert(row):
         time_string,"%d/%m/%Y %H:%M").timetuple()))
     return [timestamp, int(row[1])]
 
-def start(path):
-    with open(path, 'r',encoding="utf-8") as f:
-        reader = csv.reader(f)
-        i = 0
-        for row in reader:
-            if i > 1:
-               result = row_convert(row)
-            i += 1
-
 def printInstruction():
     print("")
     print("usage: csv_to_openstdb.py [path]")
@@ -50,4 +70,4 @@ if __name__ == '__main__':
 
     path = sys.argv[1]
 
-    start(path)
+    read_csv(path)
