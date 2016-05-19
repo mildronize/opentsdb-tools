a5aaebc36c11ef9d948b87d8afdbd05bb089cc74
Thada Wangthammang <mildronize@gmail.com>
2016-05-19 15:05:07 +0700

    Add a script for flushing data randomly into OpenTSDB with multi-thread and their tests

--- flush_data.py
+++ flush_data.py
@@ -5,25 +5,78 @@ import csv
 import time
 import datetime
 
-import requests
-import json
+import random
 
-from csv_to_opentsdb import metric_send
+import threading
 
-START_TIME = 
+from utils import *
 
-def flush_data(number_data_points):
+# timestamp in second unit
+START_TIMESTAMP = datetime_string_to_timestamp("01/01/2000 0:00")
+INTERVAL = 5 # seconds
+
+class worker (threading.Thread):
+    def __init__(self, threadID, name, start_timestamp, number_data_points):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.start_timestamp = start_timestamp
+        self.number_data_points = number_data_points
+
+        print(start_timestamp, number_data_points)
+
+    def run(self):
+        print("Starting " + self.name)
+        running_timestamp = self.start_timestamp
+        for i in range(self.number_data_points):
+            print(self.name + " timestamp: "+ str(running_timestamp) + " " + str(i/self.number_data_points*100)+ " %")
+            metric_send(metric='level', \
+                        timestamp = running_timestamp, \
+                        value = value_generator(5000, 6000), \
+                        tags = {'location':'hatyai'}
+            )
+            running_timestamp += INTERVAL
+        print("Exiting " + self.name)
+
+def value_generator(start, end):
+    return random.randint(start, end)
+
+def thread_scopes(start_timestamp, number_data_points, num_threads):
+    result = []
+    print(start_timestamp)
+    for thread_id in range(num_threads):
+        number_each_thread = int(number_data_points/num_threads)
+
+        if thread_id + 1 == num_threads and number_data_points % num_threads != 0:
+            number_each_thread_tmp = number_data_points - number_each_thread * num_threads + number_each_thread
+        else:
+            number_each_thread_tmp = number_each_thread
+
+        starting_timestamp_each_thread = start_timestamp + int(thread_id*INTERVAL*number_each_thread)
+        result.append({'num': number_each_thread_tmp, 'start': starting_timestamp_each_thread})
+    return result
+
+def flush_data(number_data_points, num_threads):
+    scopes = thread_scopes(START_TIMESTAMP, number_data_points, num_threads)
+    for i, scope in enumerate(scopes):
+        print("flush ",scope['start'], scope['num'])
+        worker(i+1, "Thread " + str(i+1), scope['start'], scope['num']).start()
     return 0
 
-
-
 def printInstruction():
+    print("flush_data.py [number_of_data_points] [num_threads]")
     pass
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    num_threads = 1
+    if len(sys.argv) < 2:
+        printInstruction()
+        exit()
+    elif len(sys.argv) == 3:
+        num_threads = int(sys.argv[2])
+    elif len(sys.argv) > 3:
         printInstruction()
         exit()
 
     number_data_points = int(sys.argv[1])
-    flush_data(number_data_points)
+    flush_data(number_data_points, num_threads)
--- tests/flush_data_test.py
+++ tests/flush_data_test.py
@@ -1,10 +1,76 @@
 import unittest
 from flush_data import *
 
+
 class FlushDataTest(unittest.TestCase):
     def flush_data_should_be_0(self):
         self.assertEqual( flush_data(5), 0)
-        
+
+class ThreadScopesTest(unittest.TestCase):
+
+    def case_1_exact_division_with_1_thread_test(self):
+        self.assertEqual(thread_scopes(0, 10,1), \
+            [
+            {'num': 10, 'start': 0}
+            ]
+        )
+
+    def case_1_exact_division_with_2_threads_test(self):
+        self.assertEqual(thread_scopes(0, 10,2), \
+            [
+            {'num': 5, 'start': 0},
+            {'num': 5, 'start': 25}
+            ]
+        )
+    def case_1_exact_division_with_3_threads_test(self):
+        self.assertEqual(thread_scopes(0, 15,3), \
+            [
+            {'num': 5, 'start': 0},
+            {'num': 5, 'start': 25},
+            {'num': 5, 'start': 50}
+            ]
+        )
+    def case_2_non_exact_division_test(self):
+        self.assertEqual(thread_scopes(0, 10,3), \
+            [
+            {'num': 3, 'start': 0},
+            {'num': 3, 'start': 15},
+            {'num': 4, 'start': 30}
+            ]
+        )
+
+    def case_1_exact_division_with_1_thread_and_start_wo_zero_test(self):
+        self.assertEqual(thread_scopes(1, 10,1), \
+            [
+            {'num': 10, 'start': 1}
+            ]
+        )
+
+    def case_1_exact_division_with_2_threads_and_start_wo_zero_test(self):
+        self.assertEqual(thread_scopes(1, 10,2), \
+            [
+            {'num': 5, 'start': 1},
+            {'num': 5, 'start': 26}
+            ]
+        )
+    def case_1_exact_division_with_3_threads_and_start_wo_zero_test(self):
+        self.assertEqual(thread_scopes(1, 15,3), \
+            [
+            {'num': 5, 'start': 1},
+            {'num': 5, 'start': 26},
+            {'num': 5, 'start': 51}
+            ]
+        )
+    def case_2_non_exact_division_and_start_wo_zero_test(self):
+        self.assertEqual(thread_scopes(1, 10,3), \
+            [
+            {'num': 3, 'start': 1},
+            {'num': 3, 'start': 16},
+            {'num': 4, 'start': 31}
+            ]
+        )
+
+
 
 # Unit
 # class PadZeroDateTest(unittest.TestCase):
