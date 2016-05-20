48bcd32cedbe98eadd1f0e4c15dcfc3f59ceda73
Thada Wangthammang <mildronize@gmail.com>
2016-05-20 20:56:17 +0700

    add 'send_metrics' for sending multiple data points per request

--- utils.py
+++ utils.py
@@ -18,6 +18,16 @@ def send_metric(metric,  value, tags, timestamp=int(time.time()) ):
         print(r.text)
         exit(1)
 
+def send_metrics(data):
+    url = 'http://127.0.0.1:4242/api/put?details'
+    r = requests.post(url, data = json.dumps(data))
+    if r.status_code != requests.codes.ok:
+        print("Error! " + str(r.status_code))
+        print(r.text)
+        # print('----- Trace Data ---------')
+        # print(json.dumps(data))
+        exit(1)
+
 def datetime_string_to_timestamp(datetime_string):
     """input string date -> 13/10/2015 0:45
     expected result ->  1444697100 """
