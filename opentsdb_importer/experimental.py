6a496b61c8df51be2223e2d2b3d2588153f123ba
Thada Wangthammang <mildronize@gmail.com>
2016-08-02 16:52:16 +0700

    add experimental with repeat and CLI

--- opentsdb_importer/experimental.py
+++ opentsdb_importer/experimental.py
@@ -2,29 +2,39 @@
 import requests
 import time
 import logging
+import collections
 logging.basicConfig(level=logging.INFO)
 
 #  from opentsdb_importer.utils import *
 import opentsdb_importer.config as config
 
-def submit_job(year, downsampling):
-    url = "http://"+config.SERVER_IP+":"+config.SERVER_PORT+"/api/query?start=2000/01/01-00:00:00&end=20%02d/01/01-00:00:00&m=sum:%s:level&show_summary=true&show_query=true" % (year, downsampling)
-    logging.info("HTTP requesting.. "+ url)
+
+def submit_job(year_start, year_end, downsampling):
+    url = "http://{}:{}/api/query?" \
+        "start=20{:02d}/01/01-00:00:00&end=20{:02d}/01/01-00:00:00" \
+        "&m=sum:{}:level&show_summary=true&show_query=true".format(config.SERVER_IP,
+                                                                   config.SERVER_PORT,
+                                                                   year_start,
+                                                                   year_end,
+                                                                   downsampling)
+    logging.info("HTTP requesting.. " + url)
     r = requests.get(url)
     if r.status_code != requests.codes.ok:
         logging.error(r.text)
         exit(1)
     return r.json()
 
-def measure(year, downsampling):
+#  submit_job(1,2,"1y-avg")
+
+
+def measure(year_start, year_end, downsampling):
     stat = {}
     #  current_stats_id = len(stats) - 1
     #  no_experimental = year
     start_time = time.time()
-    response = submit_job(year, downsampling)[1]["statsSummary"]
+    response = submit_job(year_start, year_end, downsampling)[1]["statsSummary"]
     stat["overall_time"] = (time.time() - start_time) * 1000
     # measure with this program
-    stat["no"] = year
     stat["start_time"] = start_time
     # global
     stat["processingPreWriteTime"] = \
@@ -40,8 +50,8 @@ def measure(year, downsampling):
     stat["uidToStringTime"] = \
         response["queryIdx_00"]["uidToStringTime"]
     # scanner scannerIdx_00
-    stat["compactionTime"] = \
-        response["queryIdx_00"]["scannerStats"]["scannerIdx_00"]["compactionTime"]
+    stat["compactionTime"] = response["queryIdx_00"][
+        "scannerStats"]["scannerIdx_00"]["compactionTime"]
     stat["hbaseTime"] = \
         response["queryIdx_00"]["scannerStats"]["scannerIdx_00"]["hbaseTime"]
     stat["scannerTime"] = \
@@ -49,34 +59,44 @@ def measure(year, downsampling):
 
     return stat
 
+
 def print_to_csv(stat, output, has_header):
+    ordered_stat = collections.OrderedDict(
+        sorted(stat.items(), key=lambda t: t[0]))
     with open(output, 'a') as f:
         # write header
         if has_header:
             line_output_tmp = ''
-            for key, value in stat.items():
-                line_output_tmp += ',"'+ str(key) + '"'
-            f.write(line_output_tmp[1:]+'\n')
+            for key, value in ordered_stat.items():
+                line_output_tmp += ',"' + str(key) + '"'
+            f.write(line_output_tmp[1:] + '\n')
         # write data
         line_output_tmp = ''
-        for key, value in stat.items():
-            line_output_tmp += ',"'+ str(value) + '"'
-        f.write(line_output_tmp[1:]+'\n')
+        for key, value in ordered_stat.items():
+            line_output_tmp += ',"' + str(value) + '"'
+        f.write(line_output_tmp[1:] + '\n')
+
 
 def start_experimental_increment(year_start, year_end, downsampling, output):
-    for year in range(year_start, year_end):
-        stat = measure(year, downsampling)
+    for year in range(year_start, year_end + 1):
+        stat = measure(0, year, downsampling)
 
         has_header = False
-        if year_start == year :
+        if year_start == year:
             has_header = True
         print_to_csv(stat, output, has_header)
 
-#  def start_experimental_repeat(year_start, year_end, downsampling, output):
-    #  for year in range(year_start, year_end):
-        #  stat = measure(year, downsampling)
 
-        #  has_header = False
-        #  if year_start == year :
-            #  has_header = True
-        #  print_to_csv(stat, output, has_header)
+def start_experimental_repeat(
+        year_start,
+        year_end,
+        downsampling,
+        num_repeat,
+        output):
+    for i in range(num_repeat):
+        stat = measure(year_start, year_end, downsampling)
+
+        has_header = False
+        if i == 0:
+            has_header = True
+        print_to_csv(stat, output, has_header)
